064efa6b07127385172b4135ff20fdae0f1ad12b
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-10-12 23:30:51 -0500

    device mgmt

--- ThanksForDownloading/DeviceMgmt.js
+++ ThanksForDownloading/DeviceMgmt.js
@@ -1,3 +1,5 @@
+const flowName = "device-management.json";
+
 const getFlowFileFromGH = async () => {
   const ghFlowFileURL =
     "https://api.github.com/repos/pingone-davinci/flows/contents/Solutions/CIAMPasswordless/OOTB_Device%20Management%20-%20Main%20Flow.json?ref=main";
@@ -8,13 +10,16 @@ const getFlowFileFromGH = async () => {
 };
 
 const dlFlowFileFromGH = async () => {
-  const flowJSON = await getFlowFileFromGH();
+  const flowB64 = await getFlowFileFromGH();
+  console.log("flowB64");
+  console.log(flowB64);
+  const flowJSON = JSON.parse(atob(flowB64));
   console.log("flowJSON");
   console.log(flowJSON);
-  const flowJSONStr = JSON.stringify(flowJSON);
+  const flowJSONStr = JSON.stringify(flowJSON, null, 2);
   console.log("flowJSONStr");
   console.log(flowJSONStr);
-  const blob = new Blob([flowJSON], { type: "text/plain" });
+  const blob = new Blob([flowJSONStr], { type: "text/plain" });
   const url = URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.download = "anthony---" + "device-management.json";
@@ -23,18 +28,24 @@ const dlFlowFileFromGH = async () => {
   link.click();
 };
 
+const runIfPageCompletedLoading = (event) => {
+  if (document.readyState === "complete") {
+    dlFlowFileFromGH();
+  }
+};
+
 /**
  * Wait for page to load to start the FIDO authentication process
  */
-if (document.readyState === "loading") {
+if (document.readyState !== "complete") {
   // Loading hasn't finished yet
-  document.addEventListener("DOMContentLoaded", dlFlowFileFromGH);
+  document.addEventListener("readystatechange", runIfPageCompletedLoading);
 } else {
-  // `DOMContentLoaded` has already fired
-  const dlFlowFileFromGHContinueBtn = document.getElementById(
-    "dlFlowFileFromGHContinueBtn"
-  );
-  if (dlFlowFileFromGHContinueBtn) {
-    dlFlowFileFromGHContinueBtn.addEventListener("click", dlFlowFileFromGH);
+  // Page had completed loading, ready to run function
+  const dlBtn = document.getElementById("dlBtn");
+  if (dlBtn) {
+    dlBtn.addEventListener("click", async () => {
+      await dlFlowFileFromGH();
+    });
   }
 }
